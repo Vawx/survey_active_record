334dc3ce3c1098409f90dd41b9320641eb0be9ec
kyle <altck8@gmail.com>
2016-01-26 15:44:59 -0800

    test cases for question

--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -24,3 +24,19 @@ describe Survey do
     end
   end
 end
+
+describe Question do
+  describe "#new" do
+    it 'creates a new question object' do
+      question = Question.create( {question: "yes?", survey_id: 1} )
+      expect(question.question).to(eq("yes?"))
+    end
+  end
+  describe "#survey" do
+    it 'creates a survey and question and make sure survey_id of question matches survey.id' do
+      survey = Survey.create( {name: "New Survey"} )
+      question = Question.create( {question: "yes?", survey_id: survey.id})
+      expect(question.survey_id).to(eq(survey.id))
+    end
+  end
+end
